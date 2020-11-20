782b0b3b280813d89dbcc5d6460305e8aa765f07
BhoopathiRaj <67492768+BhoopathiRaj@users.noreply.github.com>
2020-11-20 17:20:49 +0530

    Basic Programs

---
+++ Basics/variable_dec.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int
+
+	i = 10
+	var s string = "Canada"
+	name := "John Doe"
+
+	fmt.Println(i)
+	fmt.Println(s)
+	fmt.Println(name)
+	fmt.Printf("%v, %T", name, name)
+
+}
