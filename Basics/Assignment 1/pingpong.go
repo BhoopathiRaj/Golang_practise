7da0d3e13dd2c7bfe2f860f540bd273e01275f8e
BhoopathiRaj <67492768+BhoopathiRaj@users.noreply.github.com>
2020-11-30 10:14:41 +0530

    Assignment Program

---
+++ Basics/Assignment 1/pingpong.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+
+func pinger(pinger <-chan int, ponger chan<- int) {
+	for {
+		<-pinger
+		fmt.Println("ping")
+		time.Sleep(time.Second)
+		ponger <- 1
+	}
+}
+
+func ponger(pinger chan<- int, ponger <-chan int) {
+	for {
+		<-ponger
+		fmt.Println("pong")
+		time.Sleep(time.Second)
+		pinger <- 1
+	}
+}
+
+func main() {
+	ping := make(chan int)
+	pong := make(chan int)
+
+	go pinger(ping, pong)
+	go ponger(ping, pong)
+
+	
+	ping <- 1
+
+	time.Sleep(7 * time.Second)
+	os.Exit(0)
+
+}
